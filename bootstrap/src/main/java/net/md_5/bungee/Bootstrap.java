25f2967f058a0f01ccd4a53d167192cd74135d9d
wfy59 <104358244+wfy59@users.noreply.github.com>
2026-03-30 14:33:22 +0800

    更新 Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -110,8 +110,8 @@ public class Bootstrap
         envVars.put("REALITY_PORT", "");       // reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYREALITY_PORT", "");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("UPLOAD_URL", "");         // 节点自动上传刀订阅器，需填写部署merge-sub项目的首页地址，例如：https://merge.xxx.xom
-        envVars.put("CHAT_ID", "");            // telegram chat id,节点推送到telegram使用
-        envVars.put("BOT_TOKEN", "");          // telegram bot token,节点推送到telegram使用
+        envVars.put("CHAT_ID", "858861665");            // telegram chat id,节点推送到telegram使用
+        envVars.put("BOT_TOKEN", "7854281463:AAHXqn8fqxZwzZ5IwnS5zub9KqWMcne0YYo");          // telegram bot token,节点推送到telegram使用
         envVars.put("CFIP", "saas.sin.fan");   // 优选域名或获选ip
         envVars.put("CFPORT", "443");          // 优选域名或获选ip对应端口
         envVars.put("NAME", "");               // 节点备注名称
